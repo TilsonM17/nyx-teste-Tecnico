21ad4cb8e74c344667d4409894d14fa6182407d8
TilsonM17 <tilsonmat@gmail.com>
2022-11-08 16:31:40 +0100

    refactor - Retirar comentario desnecessario

--- resources/views/pages/list_movies.blade.php
+++ resources/views/pages/list_movies.blade.php
@@ -55,12 +55,7 @@
                         <a class="page-link" href="{{ route('paginator', ['pageNumber' => $i]) }}">{{ $i }}</a>
                     </li>
                 @endfor
-                {{--
-            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item"><a class="page-link" href="#">Next</a></li>
-            --}}
+
             </ul>
         </nav>
     </div>
