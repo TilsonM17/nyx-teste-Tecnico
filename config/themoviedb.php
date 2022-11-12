5eb742297e2f21d4e1162683933b3279644b7098
TilsonM17 <tilsonmat@gmail.com>
2022-11-12 16:28:59 +0100

    fix - ajustes do .env.example e das configurações

--- config/themoviedb.php
+++ config/themoviedb.php
@@ -3,7 +3,7 @@
 return [
 
     'url' => 'https://api.themoviedb.org/3/search/movie',
-    'key' => 'ccb6e406df9473408a6b5bffc32c73b0',
+    'key' => env('MOVIE_KEY'),
     'img_url' => 'https://image.tmdb.org/t/p/w200/',
     // ==========================================
 
